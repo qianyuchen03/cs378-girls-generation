b7af396858ddb8c778cbf2ec0295ceccfe3b7946
Tanya Joseph <tanyajoseph@wireless-10-148-246-77.public.utexas.edu>
2025-03-26 20:01:48 -0500

    initial card look

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar';
+import Recommendatations from './components/Recommendations';
 
 function App() {
   return (
     <div className="App">
+      <Recommendatations/>
       <Navbar/>
     </div>
   );
